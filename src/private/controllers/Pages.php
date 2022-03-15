4deeb2520f04df989453606eda0b426d43f810f3
14sourabh7 <sourabhsingh@cedcoss.com>
2022-03-15 11:35:32 +0530

    login -signup - search

--- src/private/controllers/Pages.php
+++ src/private/controllers/Pages.php
@@ -63,68 +63,6 @@ class Pages extends Controller
                 case 'getUserId':
                     $data = $this->model('Users')::find('all', array('conditions' => array('user_id = ?', $_POST['user_id'])));
                     break;
-                case 'deleteUser':
-                    $id = $_POST['user_id'];
-                    $user = $this->model('Users');
-                    $user::table()->delete(array('user_id' => array($id)));
-                    $data = $this->model('Users')::all();
-                    break;
-                case 'updateStatus':
-                    $id = $_POST['user_id'];
-                    $status = $_POST['status'];
-                    $column = $_POST['column'];
-                    $user  = $this->model('Users')::find_by_user_id($id);
-                    $user->$column = $status;
-                    $user->save();
-
-                    $data = $this->model('Users')::all();
-                    break;
-                    // blogs
-                case 'getBlogs':
-                    $data = $this->model('Blogs')::all();
-                    break;
-                case 'getBlog':
-                    $data = $this->model('Blogs')::find('all', array('conditions' => array('blog_id = ?', $_POST['blog_id'])));
-                    break;
-                case 'getUserBlog':
-                    $data = $this->model('Blogs')::find('all', array('conditions' => array('blog_id = ?', $_POST['user_id'])));
-                    break;
-                case 'addBlog':
-                    $blog = $this->model('Blogs');
-                    $blog->user_id = $_POST['user_id'];
-                    $blog->title = $_POST['title'];
-                    $blog->author = $_POST['author'];
-                    $blog->text = $_POST['text'];
-                    $blog->status = $_POST['status'];
-                    $blog->save();
-                    $data = $this->model('Blogs')::all();
-                    break;
-                case 'updateBlogStatus':
-                    $id = $_POST['blog_id'];
-                    $column = $_POST['column'];
-                    $status = $_POST['status'];
-                    $blog  = $this->model('Blogs')::find_by_blog_id($id);
-                    $blog->$column = $status;
-                    $blog->save();
-                    $data = $this->model('Blogs')::all();
-                    break;
-                case 'updateBlog':
-                    $blog_id = $_POST['blog_id'];
-                    $title = $_POST['title'];
-                    $text = $_POST['text'];
-                    $blog  = $this->model('Blogs')::find_by_blog_id($blog_id);
-                    $blog->title = $title;
-                    $blog->text = $text;
-                    $blog->save();
-                    $data = $this->model('Blogs')::all();
-                    break;
-                case 'deleteBlog':
-                    $id = $_POST['blog_id'];
-                    $user = $this->model('Blogs');
-                    $user::table()->delete(array('blog_id' => array($id)));
-                    $data = $this->model('Blogs')::all();
-                    break;
-                    break;
             }
         }
         $this->view('/pages/operation', $data);
